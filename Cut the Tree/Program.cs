151588a2e7f12be5261d69b58a8bc2e85170a987
AHMET AYDIN <ahmet.aydin@visiomex.com>
2022-06-20 19:02:12 +0300

    solving commit

---
+++ Cut the Tree/Program.cs
@@ -0,0 +1,85 @@
+﻿var data = new List<int>() { 100, 200, 100, 500, 100, 600 };
+var edges = new List<List<int>>() { new List<int>() { 1, 2 }, new List<int>() { 2, 3 }, new List<int>() { 2, 5 }, new List<int>() { 4, 5 }, new List<int>() { 5, 6 } };
+
+var result = cutTheTree(data, edges);
+
+Console.WriteLine(result);
+
+int cutTheTree(List<int> data, List<List<int>> edges)
+{
+    var idTotalDict = CalculateTotal(edges,data);
+
+    int allDataTotal = data.Sum();
+    int min = allDataTotal;
+
+    foreach (var i in idTotalDict)
+    {
+        var diff = Math.Abs(allDataTotal - 2 * i.Value);
+
+        if (min > diff)
+            min = diff;
+    }
+
+    return min;
+}
+
+Dictionary<int, int> CalculateTotal(List<List<int>> edges, List<int> data)
+{
+    var idEdgesDict = new Dictionary<int, List<int>>();
+    var idTotalDict = new Dictionary<int, int>();
+
+    idEdgesDict[edges[0][1]] = new List<int>() { edges[0][0] };
+
+    for (int i = 1; i < edges.Count(); i++)
+    {
+        if (idEdgesDict.ContainsKey(edges[i][0]))
+            idEdgesDict[edges[i][0]].Add(edges[i][1]);
+        else
+            idEdgesDict[edges[i][0]] = new List<int>() { edges[i][1] };
+
+        if (idEdgesDict.ContainsKey(edges[i][1]))
+            idEdgesDict[edges[i][1]].Add(edges[i][0]);
+        else
+            idEdgesDict[edges[i][1]] = new List<int>() { edges[i][0] };
+    }
+
+    var queue = new Queue<int>();
+
+    foreach (var i in idEdgesDict)
+    {
+        if (i.Value.Count() == 1)
+            queue.Enqueue(i.Key);
+    }
+
+    while (queue.Count() > 0)
+    {
+        var id = queue.Dequeue();
+
+        if (idEdgesDict[id].Count == 0)
+            continue;
+
+        var otherID = idEdgesDict[id][0];
+
+        if (idTotalDict.ContainsKey(id))
+            idTotalDict[id] += data[id - 1];
+        else
+            idTotalDict[id] = data[id - 1];
+
+        if (idTotalDict.ContainsKey(otherID))
+            idTotalDict[otherID] += idTotalDict[id];
+        else
+            idTotalDict[otherID] = idTotalDict[id ];
+
+        idEdgesDict[id].Remove(otherID);
+
+        if (!idEdgesDict.ContainsKey(otherID))
+            continue;
+
+        idEdgesDict[otherID].Remove(id);
+
+        if (idEdgesDict[otherID].Count() == 1)
+            queue.Enqueue(otherID);
+    }
+
+    return idTotalDict;
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Cut the Tree
+
+There is an undirected tree where each vertex is numbered from 1 to n, and each contains a data value. The sum of a tree is the sum of all its nodes' data values. If an edge is cut, two smaller trees are formed. The difference between two trees is the absolute value of the difference in their sums.Given a tree, determine which edge to cut so that the resulting trees have a minimal difference between them, then return that difference.
+Example
+data = [1, 2, 3, 4, 5, 6]
+edges = [(1, 2), (1, 3), (2, 6), (3, 4), (3,5)]
+
+The values are calculated as follows:
+
+  Edge   Tree 1  Tree 2 Absolute 
+  Cut    Sum     Sum    Difference
+  1      8       13       5
+  2      9       12       3
+  3      6       15       9
+  4      4       17       13
+  5      5       16       11
+
+The minimum absolute difference is 3.
+Note: The given tree is always rooted at vertex 1.
+
+
+Function Description 
+Complete the cutTheTree function in the editor below. cutTheTree has the following parameter(s): 
+• int data[n]: an array of integers that represent node values 
+• int edges[n-1][2]: an 2 dimensional array of integer pairs where each pair represents nodes connected by the edge 
+
+
+Returns 
+• int: the minimum achievable absolute difference of tree sums 
